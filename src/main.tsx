7cb493b35dd072c20b1384dd5ccb494ae338f8ba
Lion Puro <167165944+lionpuro@users.noreply.github.com>
2025-01-06 10:10:51 +0200

    fix: resolve typescript errors for css imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "@fontsource-variable/inter";
-import "~/index.css";
 import App from "~/App.tsx";
+import "~/index.css";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
